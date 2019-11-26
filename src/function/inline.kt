b317fa3ff0c35056d0f4993f3bef77d7c5fdfc1b
a1573595 <a1573595@gmail.com>
2019-11-26 11:15:04 +0800

    Class & Property

---
+++ src/function/inline.kt
@@ -0,0 +1,13 @@
+package function
+
+inline fun modifyString(str: String, operator:(String) -> String): String {
+    return operator(str)
+}
+
+inline fun modifyString2(str: String, noinline operator:(String) -> String): String {
+    return operator(str)
+}
+
+fun main(args: Array<String>) {
+    modifyString("Kotlin is amazing", { it.toUpperCase() })
+}
---
+++ src/function/use.kt
@@ -0,0 +1,11 @@
+package function
+
+import java.io.FileReader
+
+fun main(args: Array<String>) {
+    // AutoCloseable
+    FileReader("example.txt").use {
+        val str = it.readText()
+        println(str)
+    }
+}
